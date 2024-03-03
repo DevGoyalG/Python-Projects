7aebd824bac26828ae465afe3abb769a80f0f75f
Dev Goyal <devgoyalg2346@gmail.com>
2024-03-03 21:24:08 +0530

    Added "Calculator" Project

---
+++ calculator.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+
+print("The value of", a, "+", b, "is:", a+b)
+print("The value of", a, "-", b, "is:", a-b)
+print("The value of", a, "*", b, "is:", a*b)
+print("The value of", a, "/", b, "is:", a/b)
+print("The value of", a, "//", b, "is:", a//b)
+print("The value of", a, "%", b, "is:", a%b)
